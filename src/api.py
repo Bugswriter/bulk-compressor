6d7969f4c83d0aa514a2c7ceca04a473551674c1
Suraj Kushwah <suraj@bugswriter.com>
2024-05-04 23:52:07 +0530

    changing service name

--- src/api.py
+++ src/api.py
@@ -5,10 +5,12 @@ from src.utils import get_ftp_connection, count_dav_files
 
 app = FastAPI()
 
+SERVICE_NAME="davc"
+
 @app.get("/davc/start")
 async def start_service():
     try:
-        subprocess.run(["sudo", "systemctl", "start", "dav-compressor.service"], check=True)
+        subprocess.run(["sudo", "systemctl", "start", f"{SERVICE_NAME}.service"], check=True)
         return {"message": "Service started successfully"}
     except subprocess.CalledProcessError as _:
         raise HTTPException(status_code=500, detail="Failed to start service")
@@ -16,7 +18,7 @@ async def start_service():
 @app.get("/davc/stop")
 async def stop_service():
     try:
-        subprocess.run(["sudo", "systemctl", "stop", "dav-compressor.service"], check=True)
+        subprocess.run(["sudo", "systemctl", "stop", f"{SERVICE_NAME}.service"], check=True)
         return {"message": "Service stopped successfully"}
     except subprocess.CalledProcessError as _:
         raise HTTPException(status_code=500, detail="Failed to stop service")
